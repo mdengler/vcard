403916a8657f55e7667f424c6559d0a6dc7857bf
Martin Dengler <martin@martindengler.com>
2015-01-15 22:34:36 +0000

    VcardProperty: add __str__ method

--- vcard/vcard_property.py
+++ vcard/vcard_property.py
@@ -3,3 +3,7 @@ class VcardProperty(object):
         self.name = name
         self.parameters = None
         self.values = None
+
+    def __str__(self):
+        return "{}: parameters={}, values={}".format(
+            self.name, self.parameters, self.values)
